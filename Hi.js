906ee00bbf146597d123d3e8cae25b0a54674e14
senjataik <92498406+senjataik@users.noreply.github.com>
2021-10-14 18:22:40 +0700

    Add files via upload

---
+++ Hi.js
@@ -0,0 +1,5 @@
+<script>
+//<![CDATA[
+function downloadJSAtOnload(){var d=document.createElement("script");d.src="URL HOSTING histats.js SIMPAN DI SINI",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
+//]]>
+</script>
